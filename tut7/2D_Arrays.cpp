0f3790bd96ad7f1c12f6928c472b6c5ad53579f3
Ismael Barzani <96747803+TheBarzani@users.noreply.github.com>
2022-11-10 15:11:14 -0500

    Add files via upload

---
+++ tut7/2D_Arrays.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <string>
+
+using namespace	std;
+
+int main(){
+
+	int arr[4][5] = {  {1,2,3,4,5},
+						{5,6,7,8,6},
+						{9,10,11,1,2},
+						{2,3,5,7,8}};
+
+	// cin >> A[1] >> A[2] >> A[3] ...
+
+	
+
+	// output the values in the array
+	/*for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 5; j++){
+			cout << arr[i][j] << " " ;
+		}
+		cout << endl;
+	}*/
+
+	int num = 215215;
+	string my_text = to_string(num);
+	cout << my_text[4] << endl;
+
+
+
+}
